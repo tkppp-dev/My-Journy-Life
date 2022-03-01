424d93b124c324cf70c6a865995ef3a24c84d37f
root <taekg.park@gmail.com>
2022-03-02 01:13:59 +0900

    add jpa method for search with emailAddress

--- backend/src/main/kotlin/com/tkppp/myjournylife/member/MemberRepository.kt
+++ backend/src/main/kotlin/com/tkppp/myjournylife/member/MemberRepository.kt
@@ -3,4 +3,5 @@ package com.tkppp.myjournylife.member
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface MemberRepository : JpaRepository<Member, Long> {
+    fun findByEmailAddress(emailAddress: String): Member?
 }
