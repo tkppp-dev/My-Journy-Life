aba3b4512759a8b494be35723a88516967d4b304
root <taekg.park@gmail.com>
2022-03-10 17:32:02 +0900

    token auth를 위한 axios intercepter 등록

---
+++ frontend/src/util/_axios.js
@@ -0,0 +1,57 @@
+import axios from 'axios'
+
+const instance = axios.create({
+  baseURL: 'http://localhost:8080'
+})
+
+function getAccessToken(){
+  return ""
+}
+
+function getRefreshToken(){
+  return ""
+}
+
+instance.interceptors.request.use(
+  async config => {
+    const token = getAccessToken()
+    config.headers = {
+      'Authorization': `Bearer ${token}` 
+    }
+    return config
+  },
+  (error) => {
+    Promise.reject(error)
+  }
+)
+
+instance.interceptors.response.use(
+  response => response,
+  async (error) => {
+    const orignalRequest = error.config
+    if(error.response.status === 401 && !orignalRequest._retry){
+      console.log("AccessToken Expired.")
+
+      try{
+        const resp = await axios.post("/auth/refresh", {
+          accessToken: getAccessToken(),
+          refreshToken: getRefreshToken()
+        })
+
+        if(resp.data.success = false){
+          return resp.data.message
+        }
+        
+        // access 토큰 저장 로직 필요
+
+        orignalRequest.headers['Authorization'] = `Bearer ${resp.data.accessToken}`
+        return axios(orignalRequest)
+      } catch(error){
+        console.log(error)
+      } 
+    }
+    return Promise.reject(error)
+  }
+)
+
+export default instance
