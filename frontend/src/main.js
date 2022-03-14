1441b59829b473c332efc17e8b3c76d73dea42cb
root <taekg.park@gmail.com>
2022-03-15 00:23:40 +0900

    vuex config

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,4 +3,8 @@ import App from './App.vue'
 import store from './store'
 import router from './router'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+
+app.use(store).use(router).mount('#app')
+
+export default app
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,12 +1,36 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
+import createPersistedState from 'vuex-persistedstate'
+
+const defaultUserData = {
+  isLogin: false,
+  accessToken: null,
+  refreshToken: null,
+  info: null,
+};
 
 export default createStore({
   state: {
+    user: defaultUserData,
   },
   mutations: {
+    performLogin(state, payload) {
+      state.user.isLogin = true;
+      state.user.accessToken = payload.accessToken;
+      state.user.refreshToken = payload.refreshToken;
+    },
+    performLogout(state) {
+      state.user = defaultUserData
+    },
+    setUserInfo(state, payload) {
+      state.user.info = payload
+    },
+    setReissuedAccessToken(state, payload){
+      state.user.accessToken = payload
+    }
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {},
+  plugins: [
+    createPersistedState()
+  ]
+});
