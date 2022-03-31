6cc094746624f38bc6b54fd7e0274fdbc28b4ba9
root <taekg.park@gmail.com>
2022-03-31 22:56:22 +0900

    add redis testing controller

--- backend/src/main/kotlin/com/tkppp/myjournylife/member/controller/MemberApiController.kt
+++ backend/src/main/kotlin/com/tkppp/myjournylife/member/controller/MemberApiController.kt
@@ -2,6 +2,7 @@ package com.tkppp.myjournylife.member.controller
 
 import com.tkppp.myjournylife.dto.member.member.MemberInfoResponseDto
 import com.tkppp.myjournylife.member.service.MemberService
+import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.annotation.Secured
@@ -10,8 +11,15 @@ import org.springframework.web.bind.annotation.*
 @RestController
 @RequestMapping("/api/member")
 class MemberApiController(
-    private val memberService: MemberService
+    private val memberService: MemberService,
+    private val redisTemplate: RedisTemplate<String, String>
 ) {
+
+    @GetMapping("/test")
+    fun testRedisAtEc2(){
+        val valueOps = redisTemplate.opsForValue()
+        valueOps.set("test", "test")
+    }
 
     @GetMapping("/{emailAddress}")
     @Secured("ROLE_MEMBER")
